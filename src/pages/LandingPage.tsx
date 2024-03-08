14e2b56e8b21f64e5c6d0a7731837444479f764c
XxorinXx <orinhazan@gmail.com>
2024-03-08 22:41:27 +0200

    publish

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import peerLinkSymbol from "../assets/onlyIcon.svg";
 import peerLinkFull from "../assets/images/fullLogo.svg";
 import peerLinkLogoRounded from "../assets/logoRounded.png";
